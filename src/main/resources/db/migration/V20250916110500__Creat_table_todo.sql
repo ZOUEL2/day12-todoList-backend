ce1179e4c34a3c896eef0009959359943914a2cd
ZOUEL2 <zouel2@oocl.com>
2025-09-16 11:20:15 +0800

    init commit

---
+++ src/main/resources/db/migration/V20250916110500__Creat_table_todo.sql
@@ -0,0 +1,12 @@
+create database if not exists test_db;
+
+use test_db;
+
+create table if not exists todo (
+                                     id BIGINT auto_increment primary key,
+                                     text varchar(255) not null,
+    description text ,
+    done boolean default false,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp on update current_timestamp
+    );
